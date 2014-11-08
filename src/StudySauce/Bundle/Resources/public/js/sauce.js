f13c8fdfc7a578b9b47d1dab0d1a1dcc2904a4b5
bjcullinan <code@bjcullinan.com>
2014-11-08 00:49:45 -0700

    timing error

--- src/StudySauce/Bundle/Resources/public/js/sauce.js
+++ src/StudySauce/Bundle/Resources/public/js/sauce.js
@@ -70,36 +70,32 @@ function ssMergeScripts(content)
 }
 
 $(document).ready(function () {
+    window.sincluding = true;
     $('.sinclude').each(function () {
         var that = $(this),
             url = that.attr('data-src');
-        setTimeout(function () {
-            window.sincluding = true;
-            $.ajax({
-                url: url,
-                type: 'GET',
-                dataType: 'text',
-                success: function (data)
-                {
-                    var newStuff = $(data),
-                        styles = ssMergeStyles(newStuff),
-                        scripts = ssMergeScripts(newStuff);
-                    newStuff = newStuff.not(styles).not(scripts);
-                    // do not merge top level items with IDs that already exist
-                    newStuff.filter('[id]').each(function () {
-                        var id = $(this).attr('id');
-                        if($('#' + id).length > 0)
-                            newStuff = newStuff.not('#' + id);
-                    });
-                    that.replaceWith(newStuff);
-                    setTimeout(function () {newStuff.filter('[id]').trigger('loaded');}, 100);
-                    window.sincluding = false;
-                },
-                error: function () {
-                    window.sincluding = false;
-                }
-            });
-        }, 100);
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'text',
+            success: function (data)
+            {
+                var newStuff = $(data),
+                    styles = ssMergeStyles(newStuff),
+                    scripts = ssMergeScripts(newStuff);
+                newStuff = newStuff.not(styles).not(scripts);
+                // do not merge top level items with IDs that already exist
+                newStuff.filter('[id]').each(function () {
+                    var id = $(this).attr('id');
+                    if($('#' + id).length > 0)
+                        newStuff = newStuff.not('#' + id);
+                });
+                that.replaceWith(newStuff);
+                setTimeout(function () {newStuff.filter('[id]').trigger('loaded');}, 100);
+            },
+            error: function () {
+            }
+        });
     });
 });
 
