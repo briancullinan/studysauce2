cbe8aa7decec2cad37b5d2ca42b481bbf7de6099
bjcullinan <code@bjcullinan.com>
2014-11-18 23:36:42 -0700

    tweak to update script and course widget.

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ cd /var/www/Symfony/;
 sudo php app/console cache:clear --env=prod;
 sudo php app/console doctrine:generate:entities StudySauceBundle;
 sudo php app/console doctrine:schema:update --force;
-sudo php app/console assets:install --env=prod;
+sudo php app/console assets:install --env=prod --symlink;
 sudo php app/console assetic:dump --env=prod;
 sudo chown www-data:www-data -R app/cache/prod/
