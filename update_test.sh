e6d1d64f852534cd195f2fca81852ce1e6f6ffb3
bjcullinan <code@bjcullinan.com>
2014-11-19 19:32:34 -0700

    test update fix

--- update_test.sh
+++ update_test.sh
@@ -3,7 +3,8 @@
 cd /var/www/Symfony2/;
 sudo php app/console cache:clear --env=test;
 sudo php app/console doctrine:generate:entities StudySauceBundle;
-sudo php app/console doctrine:schema:update --force;
+sudo php app/console doctrine:schema:update --force --env=test;
 sudo php app/console assets:install --env=test --symlink;
 sudo php app/console assetic:dump --env=test;
-sudo chown www-data:www-data -R app/cache/test/
+sudo chown www-data:www-data -R app/cache/
+sudo chown www-data:www-data -R app/logs/
