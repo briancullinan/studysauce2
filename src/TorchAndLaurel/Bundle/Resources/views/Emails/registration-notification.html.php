41a0296a8f871f7d2ebdd53bcc0019b562d7cdfd
Brian Cullinan <code@bjcullinan.com>
2014-12-11 16:10:31 -0700

    registration last name

--- src/TorchAndLaurel/Bundle/Resources/views/Emails/registration-notification.html.php
+++ src/TorchAndLaurel/Bundle/Resources/views/Emails/registration-notification.html.php
@@ -2,5 +2,5 @@
 $view->extend('StudySauceBundle:Emails:layout.html.php');
 
 $view['slots']->start('message'); ?>
-A user <?php print $user->getFirst(); ?> has signed up for an account using a Torch & Laurel coupon code.
+A user <?php print $user->getFirst(); ?> <?php print $user->getLast(); ?> has signed up for an account using a Torch & Laurel coupon code.
 <?php $view['slots']->stop(); ?>
