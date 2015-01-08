6d24e5243d9e5497f5d4ca0ffe6ff78c5f021390
bjcullinan <code@bjcullinan.com>
2015-01-07 18:39:52 -0700

    updates

--- update.sh
+++ update.sh
@@ -3,6 +3,9 @@
 cd /var/www/Symfony/;
 sudo php app/console cache:clear --env=prod;
 sudo php app/console doctrine:generate:entities StudySauceBundle;
+sudo php app/console doctrine:generate:entities Course1;
+sudo php app/console doctrine:generate:entities Course2;
+sudo php app/console doctrine:generate:entities Course3;
 sudo php app/console doctrine:schema:update --force --env=prod;
 sudo php app/console assets:install --env=prod --symlink;
 sudo php app/console assetic:dump --env=prod;
